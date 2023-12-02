b466fec0b0584e864e6f7d59b2efe6e0daada11e
Ben Nash <ben@nash.org>
2023-12-02 14:22:08 -0600

    Add Day${day}.kt copy of Day01.kt for each day in December

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,7 +1,9 @@
 import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
+import kotlin.io.path.copyTo
 import kotlin.io.path.readLines
+import kotlin.io.path.writeText
 
 /**
  * Reads lines from the given input txt file.
@@ -19,3 +21,15 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+
+// create a function that copies src/Day01.kt for each day in December, renaming it to Day02.kt, Day03.kt, etc.
+fun main() {
+    val days = (1..31).map { it.toString().padStart(2, '0') }
+    days.forEach { day ->
+        Path("src/Day01.kt").copyTo(Path("src/Day$day.kt"), overwrite = true)
+        // create empty Day$day.txt and Day$day_test.txt files for each day
+        Path("src/Day$day.txt").writeText("")
+        Path("src/Day${day}_test.txt").writeText("")
+    }
+}
